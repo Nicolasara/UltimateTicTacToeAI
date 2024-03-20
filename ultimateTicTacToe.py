1310453bdfc210904185b1e02d88e5fc590c09f5
Nicolas Araujo <araujo.n@northeastern.edu>
2024-03-20 14:09:28 -0400

    Add ultimateTicTacToe implementation

--- ultimateTicTacToe.py
+++ ultimateTicTacToe.py
@@ -1,22 +1,8 @@
 from abc import abstractmethod
-
-PlayerType = 'X' | 'O'
-
-BoardState = PlayerType
-
-# TicTacToe interface
-class TicTacToe:
-    @abstractmethod
-    def get_turn(self) -> PlayerType:
-        pass
-
-    @abstractmethod
-    def get_board_copy(self) -> BoardState:
-        pass
-
-    @abstractmethod
-    def make_move(self, move: Move):
-        pass
+from simpleTicTacToe import PlayerType, BoardState, Move, Result, TicTacToe
+from ruleBook import defaultRuleBook as defaultSimpleRuleBook
+from ultimateRuleBook import UltimateRuleBook
+from ultimateTicTacToeTypes import UltimateBoardState, UltimateMove, SimpleGames
 
 # UltimateTicTacToe interface
 class UltimateTicTacToe:
@@ -25,18 +11,18 @@ class UltimateTicTacToe:
         pass
 
     @abstractmethod
-    def get_board_copy(self) -> BoardState:
+    def get_board_copy(self) -> UltimateBoardState:
         pass
 
     @abstractmethod
-    def make_move(self, move: TwoDimensionalMove):
+    def make_move(self, move: UltimateMove):
         pass
 
     @abstractmethod
-    def possible_moves(self) -> TwoDimensionalMove[]:
+    def possible_moves(self) -> list[UltimateMove]:
         pass
 
-    def  has_someone_won() -> bool:
+    def has_someone_won() -> bool:
         pass
     
     @abstractmethod
@@ -49,109 +35,140 @@ class UltimateTicTacToe:
     def result() -> Result | None:
         pass
 
-# min max algorithm
-# evaluation function 
-    
-### Example of a empty board
-emptyTicTacToeBoard1 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard2 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard3 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard4 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard5 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard6 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard7 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard8 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyTicTacToeBoard9 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-emptyUltimateTicTacToeBoard = [
-    [emptyTicTacToeBoard1, emptyTicTacToeBoard2, emptyTicTacToeBoard3],
-    [emptyTicTacToeBoard4, emptyTicTacToeBoard5, emptyTicTacToeBoard6],
-    [emptyTicTacToeBoard7, emptyTicTacToeBoard8, emptyTicTacToeBoard9]
-]
+class StrictUltimateTicTacToe(UltimateTicTacToe):
+    """A UltimateTicTacToe game that enforces the rules of the game using a passed in RuleBook.
+    """
+    def __init__(self, board: UltimateBoardState, ruleBook: UltimateRuleBook):
+        super().__init__()  
+        self.simpleGames = ultimate_board_state_to_simple_games(board)
+        self.ruleBook = ruleBook
+        self.turn = PlayerType.X
+        self.pastMove = None
 
-# Example of a board with some moves
-ticTacToeBoard1 = [
-    ['X', '', ''],
-    ['', 'O', ''],
-    ['', '', '']
-]
-ticTacToeBoard2 = [
-    ['', '', ''],
-    ['', 'X', ''],
-    ['', '', '']
-]
-ticTacToeBoard3 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ticTacToeBoard4 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ticTacToeBoard5 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ticTacToeBoard6 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ticTacToeBoard7 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ticTacToeBoard8 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ticTacToeBoard9 = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-ultimateTicTacToeBoard = [
-    [ticTacToeBoard1, ticTacToeBoard2, ticTacToeBoard3],
-    [ticTacToeBoard4, ticTacToeBoard5, ticTacToeBoard6],
-    [ticTacToeBoard7, ticTacToeBoard8, ticTacToeBoard9]
-]   
+    def get_turn(self) -> PlayerType:
+        return self.turn
+    
+    def get_board_copy(self) -> UltimateBoardState:
+        row1Copy = self.get_ultimate_board_row_copy(0)
+        row2Copy = self.get_ultimate_board_row_copy(1)
+        row3Copy = self.get_ultimate_board_row_copy(2)
+        return [row1Copy, row2Copy, row3Copy]
+    
+    def make_move(self, move: UltimateMove):
+        if (self.ruleBook.is_valid(self.get_board_copy(), move, self.pastMove)):
+            ultimateMove = move[0]
+            simpleMove = move[1]
+            simpleGame = self.simpleGames[ultimateMove[0]][ultimateMove[1]]
+            simpleGame.make_move(simpleMove, self.turn)
+            self.rotate_turn()
+        else:
+            raise Exception("Invalid move.")
+        
+    def possible_moves(self) -> list[UltimateMove]:
+        possibleMoves = []
+        for row in range(3):
+            for column in range(3):
+                simpleGame = self.simpleGames[row][column]
+                for move in simpleGame.possible_moves():
+                    possibleMoves.append(((row, column), move))
+        return possibleMoves
+    
+    def has_someone_won(self) -> bool:
+        return self.winner() != None
+    
+    def is_game_over(self) -> bool:
+        hasSomeoneWon = self.has_someone_won()
+        boardFull = self.is_board_full()
+        return hasSomeoneWon or boardFull
+    
+    def winner(self) -> PlayerType | None:
+        xThreesInARow = 0
+        oThreesInARow = 0
+        threesInARow = self.get_threes_in_a_row()
+        for games in threesInARow:
+            if self.is_three_in_a_row(games):
+                if games[0].winner() == 'X':
+                    xThreesInARow += 1
+                else:
+                    oThreesInARow += 1
+
+        if xThreesInARow == 0 and oThreesInARow == 0:
+            return None
+        elif xThreesInARow == 0:
+            return PlayerType.O
+        elif oThreesInARow == 0:
+            return PlayerType.X
+        else:
+            raise Exception("There should only be one winner, but the board seems to have multiple winners.")
+        
+    def result(self) -> Result | None:
+        if not self.is_game_over():
+            return None
+        elif self.winner() == PlayerType.X:
+            return Result.X
+        elif self.winner() == PlayerType.O:
+            return Result.O
+        else:
+            return Result.DRAW
+    
+    def get_ultimate_board_row_copy(self, row: int) -> list[TicTacToe]:
+        return [game.get_ultimate_board_row_copy() for game in self.simpleGames[row]]
+    
+    def rotate_turn(self):
+        self.turn = PlayerType.X if self.turn == PlayerType.O else PlayerType.O
+
+    def is_board_full(self) -> bool:
+        for row in self.simpleGames:
+            for game in row:
+                if not game.is_board_full():
+                    return False
+        return True
+    
+    def get_threes_in_a_row(self) -> list[list[TicTacToe]]:
+        column1 = self.get_column(0)
+        column2 = self.get_column(1)
+        column3 = self.get_column(2)
+        row1 = self.get_row(0)
+        row2 = self.get_row(1)
+        row3 = self.get_row(2)
+        diagonal1 = self.get_diagonal(1)
+        diagonal2 = self.get_diagonal(2)
+
+        return [
+            column1, column2, column3,
+            row1, row2, row3,
+            diagonal1, diagonal2
+        ]
+    
+    def get_column(self, column: int) -> list[TicTacToe]:
+        return [self.simpleGames[0][column], self.simpleGames[1][column], self.simpleGames[2][column]]
+    
+    def get_row(self, row: int) -> list[TicTacToe]:
+        return self.simpleGames[row]
+    
+    def get_diagonal(self, diagonal: int) -> list[TicTacToe]:
+        """Gets the diagonal simpleTicTacToe games of the ultimate board.
+
+        Args:
+            diagonal (int): The diagonal to get. 1 for the top-left to bottom-right diagonal, 2 for the top-right to bottom-left diagonal.
+
+        Returns:
+            list[TicTacToe]: The diagonal of simple games of the ultimate board.
+        """
+        if diagonal == 1:
+            return [self.simpleGames[0][0], self.simpleGames[1][1], self.simpleGames[2][2]]
+        else:
+            return [self.simpleGames[0][2], self.simpleGames[1][1], self.simpleGames[2][0]]
+    
+    def is_three_in_a_row(self, games: list[TicTacToe]) -> bool:
+        return games[0].winner() == games[1].winner() == games[2].winner() and games[0].winner() != None
+
+def ultimate_board_state_to_simple_games(board: UltimateBoardState) -> SimpleGames:
+    simpleGames: SimpleGames = []
+    for row in board:
+        gamesRow: list[TicTacToe] = []
+        for simpleBoard in row:
+            gamesRow.append(TicTacToe(simpleBoard, defaultSimpleRuleBook))
+        simpleGames.append(gamesRow)
+    return simpleGames
+    
