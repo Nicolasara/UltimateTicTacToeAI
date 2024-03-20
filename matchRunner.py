5c203878da0bd992fc9b20b94d862c7f35e8fede
Hunter Groff <huntergroff22@gmail.com>
2024-03-20 02:19:49 -0400

    game playing

--- matchRunner.py
+++ matchRunner.py
@@ -1,4 +1,8 @@
-def playAGame(player1, player2):
+from ultimateTicTacToe import UltimateTicTacToe
+from player import TwoDimensionalMove
+
+# play a game between two AI players and return the winner
+def playAGame(playerX, playerO, print_game = False):
     # new board state 
     game = UltimateTicTacToe()
     while not game.is_gameOver():
@@ -6,9 +10,65 @@ def playAGame(player1, player2):
         match game.get_turn:
             # p1
             case 'X':
-                move = player1(game)
+                move = playerX(game)
             # p2
             case 'O':
-                move = player2(game)
+                move = playerO(game)
         game.make_move(move)
+        if print_game:
+            print(game.get_turn() + " moves: " + str(move) + "\n")
+            print(game.toString())
+    if print_game:
+        print("Game over. Winner: " + game.winner + "\n")
     return game.winner
+
+# play a game between an AI player and a manual player
+def playAManualGame(AIplayerX):
+    game = UltimateTicTacToe()
+    while not game.is_gameOver():
+        move = None
+        match game.get_turn:
+            # p1
+            case 'X':
+                move = AIplayerX(game)
+            # p2
+            case 'O':
+                print("Enter your move: ")
+                
+                #expect entry to be: Y X y x
+                input = input().split(" ")
+                
+                #convert move to TwoDimensionalMove
+                move = TwoDimensionalMove(int(input[0]), int(input[1]), int(input[2]), int(input[3]))
+
+                #check if move is valid
+                while not game.is_valid_move(move):
+                    print("Invalid move. Try again: ")
+                    input = input().split(" ")
+                    move = TwoDimensionalMove(int(input[0]), int(input[1]), int(input[2]), int(input[3]))
+
+        game.make_move(move)
+        print(game.get_turn() + " moves: " + str(move) + "\n")
+        print(game.toString())
+    print("Game over. Winner: " + game.winner + "\n")
+    return game.winner
+
+# play a set of games between two AI and return the number of wins for each player
+def playManyGames(playerX, playerO, num_games):
+    playerX_wins = 0
+    playerO_wins = 0
+    for i in range(num_games):
+        winner = playAGame(playerX, playerO)
+        if winner == 'X':
+            playerX_wins += 1
+        elif winner == 'O':
+            playerO_wins += 1
+    return (playerX_wins, playerO_wins)
+
+#example of a player
+player1 = lambda game: game.get_valid_moves()[0]
+
+#play a game
+playAManualGame(player1)
+
+    
--- ultimateTicTacToe.py
+++ ultimateTicTacToe.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+from player import Move, TwoDimensionalMove
+from typing import List
+from enum import Enum
 
 PlayerType = 'X' | 'O'
 
@@ -6,6 +9,8 @@ BoardState = PlayerType
 
 # TicTacToe interface
 class TicTacToe:
+    board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
     @abstractmethod
     def get_turn(self) -> PlayerType:
         pass
@@ -18,8 +23,37 @@ class TicTacToe:
     def make_move(self, move: Move):
         pass
 
+    def toString(self) -> str:
+        boardString = ""
+
+        #if there is winner, print it surrounded by spaces
+        if self.winner() != None:
+            for t in range(5):
+                if t == 2:
+                    boardString += " " * 5 + self.winner() + " " * 5 + "\n"
+                else:
+                    boardString += " " * 11 + "\n"
+            return boardString
+        
+        #else, print each row
+        for r in range(3):
+            row = self.board[r]
+            #print each column in the row
+            for c in range(3):
+                col = row[c]
+                boardString += " " + col + " "
+                #divider
+                if c != 2:
+                    boardString += "|"
+            #horizontal divider
+            if r != 2:
+                boardString += "\n---|---|---\n"
+        return boardString 
+
 # UltimateTicTacToe interface
 class UltimateTicTacToe:
+    board = [[TicTacToe(), TicTacToe(), TicTacToe()], [TicTacToe(), TicTacToe(), TicTacToe()], [TicTacToe(), TicTacToe(), TicTacToe()]]
+
     @abstractmethod
     def get_turn(self) -> PlayerType:
         pass
@@ -48,6 +82,29 @@ class UltimateTicTacToe:
   
     def result() -> Result | None:
         pass
+
+    def toString(self) -> str:
+        #initialize board string
+        boardString = ""
+
+        #for each row of tic tac toe boards
+        for r in range(3):
+            gameRow = self.board[r]       
+
+            #get boards as lists of rows
+            b1 = gameRow[0].toString().split("\n")
+            b2 = gameRow[1].toString().split("\n")
+            b3 = gameRow[2].toString().split("\n")
+
+            #combine rows of each smaller board into full-width rows
+            row = ['||'.join(item) for item in zip(b1, b2, b3)]
+            for l in row:
+                boardString += l + "\n"
+
+            #if not the last row, add vertical divider
+            if r != 2:
+                boardString += "===========||===========||===========\n"
+        return boardString
 
 # min max algorithm
 # evaluation function 
