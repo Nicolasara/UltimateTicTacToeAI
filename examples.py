67043e29fd6292291fcd591acc9737f2c34638c2
Nicolas Araujo <araujo.n@northeastern.edu>
2024-03-20 14:47:10 -0400

    Add examples of tic-tac-toe boards to examples.py

---
+++ examples.py
@@ -0,0 +1,125 @@
+from simpleTicTacToeTypes import CellState
+
+### Example of a empty board
+emptySimpleBoard = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+
+### Example of a tied board
+tiedSimpleBoard = [
+    [CellState.X, CellState.O, CellState.X],
+    [CellState.O, CellState.X, CellState.O],
+    [CellState.O, CellState.X, CellState.O]
+]
+
+### Example of a won game
+wonSimpleBoard = [
+    [CellState.X, CellState.O, CellState.X],
+    [CellState.O, CellState.X, CellState.O],
+    [CellState.X, CellState.EMPTY, CellState.EMPTY]
+]
+
+emptySimpleBoard1 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard2 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard3 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard4 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard5 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard6 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard7 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard8 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptySimpleBoard9 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+emptyUltimateTicTacToeBoard = [
+    [emptySimpleBoard1, emptySimpleBoard2, emptySimpleBoard3],
+    [emptySimpleBoard4, emptySimpleBoard5, emptySimpleBoard6],
+    [emptySimpleBoard7, emptySimpleBoard8, emptySimpleBoard9]
+]
+
+# Example of a board with some moves
+ticTacToeBoard1 = [
+    [CellState.X, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.O, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard2 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.X, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard3 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard4 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard5 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard6 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard7 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard8 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ticTacToeBoard9 = [
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY],
+    [CellState.EMPTY, CellState.EMPTY, CellState.EMPTY]
+]
+ultimateTicTacToeBoard = [
+    [ticTacToeBoard1, ticTacToeBoard2, ticTacToeBoard3],
+    [ticTacToeBoard4, ticTacToeBoard5, ticTacToeBoard6],
+    [ticTacToeBoard7, ticTacToeBoard8, ticTacToeBoard9]
+]   
