7a02310d90bf844542604a5b2e4aca48bfcd48de
Nicolas Araujo <araujo.n@northeastern.edu>
2024-03-21 19:48:42 -0400

    Add a basic ultimateTicTacToe Test for possible moves

--- ultimateTicTacToe/ultimateTicTacToeTypes.py
+++ ultimateTicTacToe/ultimateTicTacToeTypes.py
@@ -6,5 +6,5 @@ UltimateBoardState = list[list[BoardState]]
 # A UltimateMove is a tuple of two Moves where the first Move is the unit board to play on and the second Move is the move to make on that unit board.
 UltimateMove = tuple[Move, Move]
 
-# A SimplGame is a list of 3 lists consisting of 3 TicTacToe games each
+# A UnitGames is a list of 3 lists consisting of 3 TicTacToe games each
 UnitGames = list[list[TicTacToe]]
